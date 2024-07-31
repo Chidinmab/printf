e606747121ea3221ddba950d9acd0a2684abda50
CyberHalidu <135424851+IbrahimKhal1@users.noreply.github.com>
2024-07-31 17:22:36 +0100

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -4,7 +4,7 @@
 
 /**
  * print_unsigned - Prints an unsigned number
- * by chidinma and halidu
+ * by chidinma and Ibrahim halidu
  * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
