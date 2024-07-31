caab08c1aec26b20010449119584bde310560aaf
CyberHalidu <135424851+IbrahimKhal1@users.noreply.github.com>
2024-07-31 17:21:26 +0100

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -3,7 +3,7 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * by chidinma and halidu
+ * by chidinma and Ibrahim halidu
  * @c: char types
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
